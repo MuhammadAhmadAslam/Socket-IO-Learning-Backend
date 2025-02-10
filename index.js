347983ba182d1e99b8f7a860f2d6e8a7748475b2
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2025-02-10 20:05:42 +0500

    learning socket

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express")
 const http = require("http")
-
+const { Server } = require("socket.io");
 
 
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
 
-server.listen(9000 , () => {
+
+io.on("connection", (socket) => {
+    console.log("A new front end connected successfully");
+    console.log(socket.id, "this is socket");
+    socket.on("user-message" , (message) => {
+        console.log("A new user message" , message);
+    })
+})
+
+server.listen(9000, () => {
     console.log("Server Started At Port" + "9000");
-    
+
 })
