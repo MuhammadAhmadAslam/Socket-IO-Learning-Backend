5a1438943aa0e36782e10cdcfd63b54d30605e0c
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2025-02-11 23:25:08 +0500

    learning

--- index.js
+++ index.js
@@ -13,15 +13,27 @@ const io = new Server(server, {
 });
 
 
-io.on("connection", (socket) => {
-    console.log("A new front end connected successfully");
-    console.log(socket.id, "this is socket");
-    socket.on("user-message" , (message) => {
-        console.log("A new user message" , message);
+// io.on("connection", (socket) => {
+//     console.log("A new front end connected successfully");
+//     console.log(socket.id, "this is socket");
+//     socket.on("user-message" , (message) => {
+//         console.log("A new user message" , message);
+//     })
+//     socket.broadcast.emit(("meesage" , (message) => {
+//         console.log("A new user joined");
+//     }))
+// })
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.broadcast.emit("welcome" , "Welcome New User " + socket.id);
+    socket.on("disconnect" , () => {
+        console.log("Socket Disconnected Successfully" , socket.id);
     })
-})
+});
+
 
 server.listen(9000, () => {
-    console.log("Server Started At Port" + "9000");
+    console.log("Server Started At Port" + " 9000");
 
 })
